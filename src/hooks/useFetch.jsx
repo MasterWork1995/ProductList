954bbab80d05626f95ac3b47a844402b6e30e8bf
Yurii Yurents <yura19950117@gmail.com>
2023-10-11 23:55:32 +0300

    Fix useFetch

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,24 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://dummyjson.com/products';
 
-const useFetch = (url, page) => {
+const useFetch = (url) => {
+  const isInitialRender = useRef(true);
   const [data, setData] = useState([]);
   const [total, setTotal] = useState(null);
   const [loading, setLoading] = useState(false);
-  const params = page !== undefined ? { limit: 12, skip: page * 12 } : {};
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await axios.get(url, { params });
-        if (page === undefined) {
-          setData(response.data);
+        const response = await axios.get(url);
+
+        if (response.data.products) {
+          setData((prev) => [...prev, ...response.data.products]);
         } else {
-          setData([...data, ...response.data.products]);
+          setData(response.data);
         }
+        console.log(response);
+
         if (!total) {
           setTotal(response.data.total);
         }
@@ -26,11 +29,15 @@ const useFetch = (url, page) => {
         console.log(`Error: ${error}`);
       } finally {
         setLoading(false);
+        isInitialRender.current = true;
       }
     };
 
-    fetchData();
-  }, [url, page]);
+    if (isInitialRender.current) {
+      isInitialRender.current = false;
+      fetchData();
+    }
+  }, [url]);
 
   return [data, total, loading];
 };
