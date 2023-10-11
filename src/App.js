41d851cbf6f72f2d1c04dd90cbc60d558585b8f8
Yurii Yurents <yura19950117@gmail.com>
2023-10-11 20:35:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import routes from './routes';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="ProductList">
       <Header />
       <Container>
         <Routes>
